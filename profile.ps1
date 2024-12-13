e00250a2bc876e82843e6683fab8ced3e3039206
Chriswalt64 <walters.chris64@gmail.com>
2024-12-13 10:07:50 -0500

    initial

---
+++ profile.ps1
@@ -0,0 +1,22 @@
+$Host.UI.RawUI.WindowTitle="The Deep Magicks"
+Add-Type -AssemblyName presentationCore
+$fartSound = New-Object system.windows.media.mediaplayer
+
+function gitfart {
+	git push origin main 
+	$fartSound.open('brain-fart.mp3')
+	$fartSound.Play()
+	echo "Repo: shitted"
+	}
+	$fartSound.Close()
+	
+function gitgimme {
+
+	param ([string]$Url)
+	git clone $Url
+	$fartSound.open('gimme-fuel.mp3')
+	$fartSound.Play()
+	echo "Git got Gimme'd"
+	$fartSound.Close()
+}
+	
